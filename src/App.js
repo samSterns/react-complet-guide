041db78547c9be76b01c854876e52ecffdd67efc
samSterns <sam.g.sterns@gmail.com>
2020-12-04 11:49:41 -0800

    section 4

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ class App extends Component {
         { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
-      ]
+      ],
+      otherState: 'some other value',
+      showPersons: false
     });
   };
 
@@ -34,28 +36,43 @@ class App extends Component {
     });
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid red',
+      cursor: 'pointer'
+    }
+
+    let persons = null 
+
+    if (this.state.showPersons) {
+      persons = ( 
+          <div>
+            {this.state.persons.map(person => {
+              return <Person 
+                  name={person.name}
+                  age={person.age}
+                />
+            })}
+          </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!!')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!!!')} 
-          changed={this.nameChangedHandler}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+       {persons}
+          
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
